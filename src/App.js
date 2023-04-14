bbfcf6b508d4d1b38a468fbfab16525ec8d30a04
Saurabh Singh <saurabh.rgt@gmail.com>
2023-04-14 18:48:27 +0530

    14th-april

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import "./App.css";
 import { useState } from "react";
+import Square from "./components/Square";
 
 function App() {
   const [board, setBord] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("x");
+  const chooseSquare = (square) => {
+    setBord(
+      board.map((val, idx) => {
+        if (idx == square) {
+          return player;
+        }
 
-  return <div className="App"></div>;
+        return val;
+      })
+    );
+
+    if (player == "x") {
+      setPlayer("o");
+    } else {
+      setPlayer("x");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+        </div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
